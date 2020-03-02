eec43141e9438e7e5ee45c42c6b4fca77aeb8b2d
ravihabibillah <ravihabibillah44@gmail.com>
2020-03-02 13:16:05 +0700

    second commit

--- app/src/main/java/com/example/myapplication/favoriteFragment.java
+++ app/src/main/java/com/example/myapplication/favoriteFragment.java
@@ -4,16 +4,23 @@ package com.example.myapplication;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import java.util.ArrayList;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class favoriteFragment extends Fragment {
+
+    private RecyclerView rvHero;
+    private ArrayList<ModelHero> listHero = new ArrayList<>();
 
     public favoriteFragment() {
         // Required empty public constructor
@@ -24,7 +31,22 @@ public class favoriteFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_favorite, container, false);
+        View tampil =  inflater.inflate(R.layout.fragment_favorite, container, false);
+
+        rvHero = tampil.findViewById(R.id.rvHero);
+        rvHero.setHasFixedSize(true);
+        listHero.addAll(DataHero.getListHero());
+
+        show();
+
+        return tampil;
+    }
+
+    private void show() {
+        rvHero.setLayoutManager(new LinearLayoutManager(getActivity()));
+        HeroAdapter heroAdapter = new HeroAdapter(getActivity());
+        heroAdapter.setModelHeroes(listHero);
+        rvHero.setAdapter(heroAdapter);
     }
 
 }
